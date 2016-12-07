e60db60f8e0ef00215e31d37a7d1f817f71035c1
KevinLaw <kevinlaw@thinkgeo.com>
2016-12-07 18:16:19 +0800

    Init project

---
+++ WorldStreetsRenderLayerSample/Form.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WorldStreetsRenderLayerSample
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.winformsMap = new ThinkGeo.MapSuite.WinForms.WinformsMap();
+            this.SuspendLayout();
+            // 
+            // winformsMap
+            // 
+            this.winformsMap.BackColor = System.Drawing.Color.White;
+            this.winformsMap.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.Default;
+            this.winformsMap.CurrentScale = 590591790D;
+            this.winformsMap.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.winformsMap.DrawingQuality = ThinkGeo.MapSuite.Drawing.DrawingQuality.Default;
+            this.winformsMap.Location = new System.Drawing.Point(0, 0);
+            this.winformsMap.MapFocusMode = ThinkGeo.MapSuite.WinForms.MapFocusMode.Default;
+            this.winformsMap.MapResizeMode = ThinkGeo.MapSuite.Shapes.MapResizeMode.PreserveScale;
+            this.winformsMap.MapUnit = ThinkGeo.MapSuite.GeographyUnit.DecimalDegree;
+            this.winformsMap.MaximumScale = 80000000000000D;
+            this.winformsMap.MinimumScale = 200D;
+            this.winformsMap.Name = "winformsMap";
+            this.winformsMap.Size = new System.Drawing.Size(1008, 729);
+            this.winformsMap.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.Default;
+            this.winformsMap.TabIndex = 0;
+            this.winformsMap.Text = "winformsMap";
+            this.winformsMap.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+            this.winformsMap.ThreadingMode = ThinkGeo.MapSuite.WinForms.MapThreadingMode.Default;
+            this.winformsMap.ZoomLevelSnapping = ThinkGeo.MapSuite.WinForms.ZoomLevelSnappingMode.Default;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.winformsMap);
+            this.Name = "Form";
+            this.Text = "World Streets Render";
+            this.Load += new System.EventHandler(this.FormLoad);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ThinkGeo.MapSuite.WinForms.WinformsMap winformsMap;
+    }
+}
+
---
+++ WorldStreetsRenderLayerSample/Form.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Configuration;
+using System.Windows.Forms;
+using ThinkGeo.MapSuite;
+using ThinkGeo.MapSuite.Layers;
+using ThinkGeo.MapSuite.Shapes;
+using ThinkGeo.MapSuite.WinForms;
+
+namespace WorldStreetsRenderLayerSample
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private string sourceDataDirectory = ConfigurationManager.AppSettings["SourceDataDirectory"];
+
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void FormLoad(object sender, EventArgs e)
+        {
+            winformsMap.MapUnit = GeographyUnit.Meter;
+            winformsMap.CurrentExtent = new RectangleShape(-10801705.3653354, 3895575.29512613, -10750034.0634737, 3838706.28834338);
+
+            LayerOverlay layerOverlay = new LayerOverlay();
+            WorldStreetsRenderLayer worldStreetsRenderLayer = new WorldStreetsRenderLayer(sourceDataDirectory);
+            layerOverlay.Layers.Add(worldStreetsRenderLayer);
+            winformsMap.Overlays.Add(layerOverlay);
+            winformsMap.Refresh();
+        }
+    }
+}
